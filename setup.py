db93bcdc19f57ae84734b7c6a35e022b82c136dd
Roy Wellington Ⅳ <cactus_hugged@yahoo.com>
2013-08-27 16:39:53 -0700

    setup.py.

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='NBT',
+    version='1.0',
+    description='NBT (the format used by Minecraft) encoder/decoder.',
+    author='Roy Wellington',
+    url='https://github.com/thanatos/nbt',
+    packages=['nbt']
+)
